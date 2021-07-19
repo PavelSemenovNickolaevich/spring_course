05ef4b8eb28f3b21d7bce795a05534521177d33c
Pavel Semenov <PNSemenov@sberbank.ru>
2021-07-19 08:23:21 +0300

    Inversion of Control and Dependency injection

---
+++ src/main/java/spring_introduction/Person.java
@@ -0,0 +1,46 @@
+package spring_introduction;
+
+public class Person {
+
+    private Pet pet;
+    private String surname;
+    private String age;
+
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+    public Person() {
+        System.out.println("Person bean is created");
+    }
+
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
+        this.pet = pet;
+    }
+
+    public void callYourPet() {
+        System.out.println("Hello");
+        pet.say();
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class person: set surname");
+        this.surname = surname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        System.out.println("Class person: set age");
+        this.age = age;
+    }
+}
