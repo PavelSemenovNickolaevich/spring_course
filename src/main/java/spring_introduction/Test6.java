5c384ab1a594a430f01e1e2a19b00f60ef065495
Pavel Semenov <PNSemenov@sberbank.ru>
2021-07-20 08:24:33 +0300

    конфигурация с помощью джава кода Способ 2

--- src/main/java/spring_introduction/Test6.java
+++ src/main/java/spring_introduction/Test6.java
@@ -7,6 +7,10 @@ public class Test6 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
+//        Pet cat1 = context.getBean("catBean", Pet.class);
+//        Pet cat2 = context.getBean("catBean", Pet.class);
+        //cat.say();
+
         Person person = context.getBean("personBean", Person.class);
         person.callYourPet();
 
