86ebac3a488609acc37ff35138ec8df1310b6cbf
Pavel Semenov <PNSemenov@sberbank.ru>
2021-07-23 08:36:18 +0300

    Advice типа @Around, работа с исключениями

--- src/main/java/aop/aspects/NewLoggingAspect.java
+++ src/main/java/aop/aspects/NewLoggingAspect.java
@@ -13,11 +13,17 @@ public class NewLoggingAspect {
     @Around("execution(public String returnBook())")
     public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("aroundReturnBookLoggingAdvice:  в библиотеку пытаются вернуть книгу");
-        long begin = System.currentTimeMillis();
-        Object targetMethodResult = proceedingJoinPoint.proceed();
-        long end = System.currentTimeMillis();
+//        long begin = System.currentTimeMillis();
+        Object targetMethodResult = null;
+        try {
+             targetMethodResult = proceedingJoinPoint.proceed();
+        } catch (Exception e) {
+            System.out.println("aroundReturnBookLoggingAdvice: было поймано исключение " + e);
+            targetMethodResult = "Неизвстное название книги";
+        }
+//        long end = System.currentTimeMillis();
         System.out.println("aroundReturnBookLoggingAdvice:  в библиотеку успешно вернули книгу");
-        System.out.println("aroundReturnBookLoggingAdvice:  метод returnBook выполнил работу за " + (end - begin) + " милисекунд");
+//        System.out.println("aroundReturnBookLoggingAdvice:  метод returnBook выполнил работу за " + (end - begin) + " милисекунд");
         return targetMethodResult;
     }
 }
